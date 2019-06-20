57601bb46492e2c621458b40870a76c3caaaa3dd
Antanas Kompanas <a.kompanas@gofintech.lt>
2019-06-20 16:35:46 +0300

    Copyright notice and typo fix.

--- src/HttpServiceClientBase.php
+++ src/HttpServiceClientBase.php
@@ -1,5 +1,13 @@
 <?php
 
+/*
+ * This file is part of the Allegro framework.
+ *
+ * (c) 2019 Go Financial Technologies, JSC
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace GoFinTech\Allegro\Http;
 
@@ -20,7 +28,7 @@ class HttpServiceClientBase
     public function __construct(string $endpoint, ?SerializerInterface $serializer = null)
     {
         $this->httpClient = new GuzzleClient([
-            'base_uri' => $endpoint ?? 'http://internal-history-svc/'
+            'base_uri' => $endpoint
         ]);
         $this->serializer = $serializer ?? SerializerFactory::create();
     }
