705795a4c5b521514db024616ee715106b21ed96
Antanas Kompanas <a.kompanas@gofintech.lt>
2019-07-15 17:07:24 +0300

    Some documentation for proxy client factory.

--- src/HttpServiceClientFactory.php
+++ src/HttpServiceClientFactory.php
@@ -1,5 +1,13 @@
 <?php
 
+/*
+ * This file is part of the Allegro framework.
+ *
+ * (c) 2019 Go Financial Technologies, JSC
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace GoFinTech\Allegro\Http;
 
@@ -8,6 +16,14 @@ use GoFinTech\Allegro\Http\Implementation\ServiceClientProxy;
 use ProxyManager\Factory\RemoteObjectFactory;
 use Symfony\Component\Serializer\SerializerInterface;
 
+/**
+ * HttpServiceClientFactory creates HTTP service client automatically.
+ * More specifically, it creates a dynamic interface proxy
+ * that uses HttpServiceClient to call methods over HTTP.
+ * Wire API matches the JsonServiceHandler on the server side.
+ *
+ * @package GoFinTech\Allegro\Http
+ */
 class HttpServiceClientFactory
 {
     /** @var SerializerInterface|null */
--- src/Implementation/ServiceClientProxy.php
+++ src/Implementation/ServiceClientProxy.php
@@ -1,5 +1,13 @@
 <?php
 
+/*
+ * This file is part of the Allegro framework.
+ *
+ * (c) 2019 Go Financial Technologies, JSC
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace GoFinTech\Allegro\Http\Implementation;
 
@@ -11,6 +19,11 @@ use ReflectionClass;
 use ReflectionException;
 use Symfony\Component\Serializer\SerializerInterface;
 
+/**
+ * Adapter for Ocramius ProxyManager for RemoteObject type calls.
+ *
+ * @package GoFinTech\Allegro\Http\Implementation
+ */
 class ServiceClientProxy extends HttpServiceClientBase implements AdapterInterface
 {
     /** @var string[] indexed by method name */
